316cfc74a3edc1be7abef15053b95549e8c26d55
Divyanshibhatt <divyanshib2@gmail.com>
2026-03-29 14:58:54 +0530

    Delete translation.py

--- translation.py
+++
@@ -1,5 +0,0 @@
-from utils import call_model
-def translation_agent(text):
-    prompt = f"Convert this into Hinglish:\n{text}"
-    res = call_model([{"role":"user","content": prompt}])
-    return res.choices[0].message.content.strip()
